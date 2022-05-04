3316538a02adc93462b2ead1f967d152952d7c9c
Fernando Benavides <nockzblack@gmail.com>
2022-05-04 17:16:03 -0500

    Initial Commit

---
+++ HealthKitSteps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HealthKitSteps
+//
+//  Created by Fernando's Mac on 04/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HealthKitSteps/HealthKitStepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitStepsApp.swift
+//  HealthKitSteps
+//
+//  Created by Fernando's Mac on 04/05/22.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitStepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

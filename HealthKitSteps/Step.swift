b0dd8c5ade7b2d83d0e2730baa83dc36a7552ccc
Fernando Benavides <nockzblack@gmail.com>
2022-05-04 17:56:33 -0500

    Step Model

---
+++ HealthKitSteps/Step.swift
@@ -0,0 +1,17 @@
+//
+//  Step.swift
+//  HealthKitSteps
+//
+//  Created by Fernando's Mac on 04/05/22.
+//
+
+import Foundation
+
+
+struct Step: Identifiable {
+    let id = UUID()
+    let count: Int
+    let date: Date
+    
+    
+}

a0c7743e8021b80b885f2a4940590273fd324f11
Fernando Benavides <nockzblack@gmail.com>
2022-05-04 17:57:19 -0500

    View edited to populate according model

--- HealthKitSteps/ContentView.swift
+++ HealthKitSteps/ContentView.swift
@@ -6,11 +6,53 @@
 //
 
 import SwiftUI
+import HealthKit
 
 struct ContentView: View {
+    
+    private var healStore: HealStore?
+    @State private var steps: [Step] = [Step]()
+    
+    init() {
+        healStore = HealStore()
+    }
+    
+    private func updateUIFromStatistics(_ statisticsCollection: HKStatisticsCollection) {
+        let startDate = Calendar.current.date(byAdding: .day, value: -7, to: Date())!
+        
+        let endDate = Date()
+        
+        statisticsCollection.enumerateStatistics(from: startDate, to: endDate) { (statistics, stop) in
+            let count = statistics.sumQuantity()?.doubleValue(for: .count())
+            
+            let step = Step(count: Int(count ?? 0), date: statistics.startDate)
+            
+            steps.append(step)
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(steps, id: \.id) { step in
+            VStack {
+                Text("\(step.count)")
+                Text(step.date, style: .date)
+                    .opacity(0.5)
+            }
+        }
+        .padding()
+        .onAppear {
+            if let healStore = healStore {
+                healStore.requestAuthorization { success in
+                    if success {
+                        healStore.calculateSteps { statisticsCollection in
+                            if let statisticsCollection = statisticsCollection {
+                                updateUIFromStatistics(statisticsCollection)
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
